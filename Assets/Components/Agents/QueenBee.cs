b4840a7c73361209ca0742863c0802fefd92c186
loveWingBell <124731128+loveWingBell@users.noreply.github.com>
2026-02-11 13:26:03 -0700

    set up ant classes

---
+++ Assets/Components/Agents/QueenBee.cs
@@ -0,0 +1,16 @@
+// CPSC 565 Assignment 3 - Elda Britu - 30158734 - February 13, 2025
+
+using UnityEngine;
+using Antymology.Terrain;
+
+namespace Antymology.Agents
+{
+    /// <summary>
+    /// The one queen ant per generation.
+    /// Should probably be a larger gold/yellow sphere. (will change later if it doesn't look good)
+    /// Gene[2]: moves toward high-mulch areas.
+    /// Places a nest block every N ticks if she has enough health.
+    /// </summary>
+    public class QueenAnt : AntBase
+    {}
+}
---
+++ Assets/Components/Agents/WorkerBee.cs
@@ -0,0 +1,13 @@
+// CPSC 565 Assignment 3 - Elda Britu - 30158734 - February 13, 2025
+
+using UnityEngine;
+
+namespace Antymology.Agents
+{
+    /// <summary>
+    /// A worker ant. Gene[2] makes it move toward the queen.
+    /// Should probably be a dark red cube . (will change later if it doesn't look good)
+    /// </summary>
+    public class WorkerAnt : AntBase
+    {}
+}
